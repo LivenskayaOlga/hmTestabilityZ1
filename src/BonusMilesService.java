19f5a8b038be43fe91e110c5119562315636f82c
Livenskaya Olga <livenskaya@list.ru>
2023-07-26 14:38:03 +0300

    Add program

---
+++ src/BonusMilesService.java
@@ -0,0 +1,9 @@
+public class BonusMilesService {
+        public int calculate (int prise) {
+            int prMiles = 20;
+            int bonusMiles = prise / prMiles;
+
+            return bonusMiles;
+
+        }
+}
